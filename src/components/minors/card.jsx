d9b6567bda712ae02980d8935c0bbb63a848d81f
Ishan Sharma <ishandeveloper@outlook.com>
2020-04-30 01:47:21 +0530

    Added Cards

---
+++ src/components/minors/card.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+
+function Card() {
+    return <div>
+      <div class="col s12 m3">
+        <div class="card white lighten">
+          <div class="card-content black-text">
+            <span class="card-title">Title</span>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin et
+              lorem facilisis, scelerisque ipsum vel, sodales lectus. Nunc
+              rutrum et arcu quis ullamcorper. In hac habitasse platea dictumst.
+              In commodo tortor at dui condimentum, id cursus quam interdum.
+              Integer vitae nibh sapien. Morbi tempus commodo erat id dictum.
+            </p>
+          </div>
+          <div class="card-action"></div>
+        </div>
+      </div>
+    </div>
+}
+
+export default Card;
