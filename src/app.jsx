d6288b09f3a5f74ab975f19bb0c6c2620b06bede
Ishan Sharma <ishandeveloper@outlook.com>
2020-04-30 01:24:54 +0530

    App

---
+++ src/app.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Header from "./components/header";
+import Note from "./components/note";
+import Footer from "./components/footer";
+function App() {
+  return (
+    <div>
+      <Header />
+      <Note />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
