ba62279331d80953a87d03ca3bff20fd2d63acfa
Ishan Sharma <ishandeveloper@outlook.com>
2020-04-30 02:38:17 +0530

    Bug Fixes

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -1,15 +1,15 @@
 import React from "react";
 
-const year=new Date().getFullYear();
+// const year=new Date().getFullYear();
 
 function Footer() {
   return (
     <div>
-      <footer class="blue lighten-4 page-footer">
-          <div class="blue lighten-2 footer-copyright">
-            <div class="container white-text text-lighten-2 ">
+      <footer className="blue lighten-4 page-footer">
+          <div className="blue lighten-2 footer-copyright">
+            <div className="container white-text text-lighten-2 ">
             <center>
-            <a class="white-text text-lighten-2 middle" href="https://ishandeveloper.com">Made with ♥ by <b>ishandeveloper</b></a>
+            <a className="white-text text-lighten-2 middle" href="https://ishandeveloper.com">Made with ♥ by <b>ishandeveloper</b></a>
             </center>
             </div>
           </div>
