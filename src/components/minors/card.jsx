6f0b4511aea0d919a013b3c327a6a47070aaa5f4
Ishan Sharma <ishandeveloper@outlook.com>
2020-04-30 03:14:32 +0530

    Modularity

--- src/components/minors/card.jsx
+++ src/components/minors/card.jsx
@@ -1,17 +1,13 @@
 import React from 'react';
 
-function Card() {
+function Card(props) {
     return <div>
       <div className="col s12 m3">
         <div className="card white lighten">
           <div className="card-content black-text">
-            <span className="card-title">Title</span>
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin et
-              lorem facilisis, scelerisque ipsum vel, sodales lectus. Nunc
-              rutrum et arcu quis ullamcorper. In hac habitasse platea dictumst.
-              In commodo tortor at dui condimentum, id cursus quam interdum.
-              Integer vitae nibh sapien. Morbi tempus commodo erat id dictum.
+            <span className="card-title">{props.title}</span>
+            <p className="text-justify">
+              {props.content}
             </p>
           </div>
           <div className="card-action"></div>
--- src/components/note.jsx
+++ src/components/note.jsx
@@ -1,10 +1,46 @@
 import React from "react";
-import Card from './minors/card';
+import Card from "./minors/card";
 
 function Notes() {
   return (
     <div className="row">
-        <Card/><Card/><Card/><Card/><Card/>
+      <Card
+        title="First Note"
+        content="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin et
+        lorem facilisis, scelerisque ipsum vel, sodales lectus. 
+        In commodo tortor at dui condimentum, id cursus quam interdum.
+        Integer vitae nibh sapien. Morbi tempus commodo erat id dictum."
+      />
+      <Card
+        title="First Note"
+        content="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin et
+        lorem facilisis, scelerisque ipsum vel, sodales lectus. Nunc
+        rutrum et arcu quis ullamcorper. In hac habitasse platea dictumst.
+        In commodo tortor at dui condimentum, id cursus quam interdum.
+        Integer vitae nibh sapien. Morbi tempus commodo erat id dictum."
+      />
+      <Card
+        title="First Note"
+        content="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin et
+        lorem facilisis, scelerisque ipsum vel, sodales lectus. Nunc
+        rutrum et arcu quis ullamcorper. In hac habitasse platea dictumst.
+        In commodo tortor at dui condimentum, id cursus quam interdum.
+        Integer vitae nibh sapien. Morbi tempus commodo erat id dictum."
+      />
+      <Card
+        title="First Note"
+        content="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin et
+        lorem facilisis, scelerisque ipsum vel, sodales lectus. Nunc
+        rutrum et arcu quis ullamcorper. In hac habitasse platea dictumst.
+        In commodo tortor at dui condimentum, id cursus quam interdum.
+        Integer vitae nibh sapien. Morbi tempus commodo erat id dictum."
+      />
+      <Card
+        title="First Note"
+        content="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin et
+        lorem facilisis, scelerisque ipsum vel, sodales lectus. Nunc
+        rutrum et arcu quis ullamcorper. "
+      />
     </div>
   );
 }
