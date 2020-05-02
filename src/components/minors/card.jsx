49ceb8181696f3fc9ade4f312a3d9aab7f8d016c
Ishan Sharma <ishandeveloper@outlook.com>
2020-05-02 13:18:10 +0530

    Adding & Deleting Notes

--- src/components/minors/card.jsx
+++ src/components/minors/card.jsx
@@ -1,16 +1,15 @@
 import React from "react";
 
-function createCard(content) {
-  return (
-    <Card
-      key={content.id}
-      title={content.title}
-      content={content.note}
-    />
-  );
+function createCard(content,deletenote) {
+  return <Card del={deletenote} id={content.id} id={content.id} key={content.id} title={content.title} content={content.note} />;
 }
 
 function Card(props) {
+
+  function taskDelete() {
+    props.del(props.id)
+  }
+
   return (
     <div>
       <div className="col s12 m3">
@@ -19,7 +18,33 @@ function Card(props) {
             <span className="card-title">{props.title}</span>
             <p className="text-justify">{props.content}</p>
           </div>
-          <div className="card-action"></div>
+          <div className="card-action blue-text">
+            <a
+              style={{
+                display: "flex",
+                flexDirection: "row-reverse",
+                marginRight: "5px",
+              }}
+              className="blue-text text-lighten-3"
+              href="#"
+            >
+              <i onClick={taskDelete}
+                className="small material-icons tooltipped"
+                data-position="top"
+                data-tooltip="Delete"
+              >
+                delete
+              </i>
+              <i
+                style={{ marginRight: "10px", fontSize: "1.8em" }}
+                className="small material-icons tooltipped"
+                data-position="top"
+                data-tooltip="Copy To Clipboard"
+              >
+                file_copy
+              </i>
+            </a>
+          </div>
         </div>
       </div>
     </div>
--- src/components/newNote.jsx
+++ src/components/newNote.jsx
@@ -1,19 +1,31 @@
-import React from "react";
+import React, {useState} from "react";
 
-function NewNote() {
+function NewNote(props) {
   const [textareaStyle, settextareaStyle] = React.useState({
     textAlign: "justfiy",
   });
+  const [content, setcontent] = useState('')
+  const [title, settitle] = useState('');
+  
+  function newnoteadded() {
+      {props.add(title,content)};
+      settitle('');
+      setcontent('');
+  }
 
   function textareaHeight(e) {
-    console.log("INPUT");
     settextareaStyle({
       height: e.target.scrollHeight + "px",
       textAlign: "justfiy",
       maxHeight: "500px",
     });
   }
-
+  function titlechanged(e) {
+      settitle(e.target.value);
+  }
+  function contentchanged(e) {
+    setcontent(e.target.value);
+}
   return (
     <div className="newnote">
       <div className="col s12 m6">
@@ -23,10 +35,12 @@ function NewNote() {
           </div>
           <div className="card-content">
             <div className="input-title">
-              <input id="email" type="note" placeholder="Title" />
+              <input id="email" type="note" onChange={titlechanged} value={title} placeholder="Title" />
             </div>
             <p>
               <textarea
+              value={content}
+              onChange={contentchanged}
                 style={textareaStyle}
                 onInput={textareaHeight}
                 placeholder="Take a note..."
@@ -35,7 +49,7 @@ function NewNote() {
               ></textarea>
             </p>
           </div>
-          <a className="btn-floating btn-large halfway-fab waves-effect waves-dark blue">
+          <a onClick={newnoteadded} className="btn-floating btn-large halfway-fab waves-effect waves-dark blue">
             <i className="material-icons">add</i>
           </a>
         </div>
