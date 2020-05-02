1e01fdda90b26b23c77f0af9f512346590a3d999
Ishan Sharma <ishandeveloper@outlook.com>
2020-05-02 13:17:53 +0530

    Functionality

--- src/app.jsx
+++ src/app.jsx
@@ -1,12 +1,40 @@
 import React from "react";
 import Header from "./components/header";
-import Note from "./components/note";
+import createCard from "./components/minors/card";
 import Footer from "./components/footer";
+import NewNote from "./components/newNote";
+// import notes from './notes';
 function App() {
+  const [notes, setnotes] = React.useState([
+    {
+      id: 0,
+      title: "First Note",
+      note:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi elementum dictum tincidunt. Nullam in convallis lectus. Donec vestibulum sagittis lectus, sit amet posuere odio pharetra ac. Nullam pulvinar orci vel aliquet malesuada. Nulla maximus, lorem non rutrum viverra, mauris mi interdum neque, id commodo dui ante sit amet nisi.  ",
+    },
+  ]);
+
+  function addNote(title, content) {
+    const newId=notes.length;
+    setnotes((prev) => {
+      return [
+        ...prev,
+        {
+          id:newId,
+          title: title,
+          note: content,
+        },
+      ];
+    });
+  }
+  function deleteNote(id) {
+    setnotes((prevNotes) => prevNotes.filter((note, index) => index !== id));
+  }
   return (
     <div>
       <Header />
-      <Note />
+      <NewNote add={addNote}/>
+      <div className="row">{notes.map((note)=>createCard(note,deleteNote))}</div>
       <Footer />
     </div>
   );
